3b458fb00375af6be9b68bdfc087874d7c0accdf
ankit9607 <ankit9607@gmail.com>
2023-09-08 15:50:01 +0530

    updated deliver.sh script

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -7,8 +7,9 @@ echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
 npm run build
-npm install serve
-serve -s build -l 3000
+# npm install serve
+# serve -s build -l 3000
+npm start
 set +x
 
 # echo 'The following "npm" command runs your Node.js/React application in'
